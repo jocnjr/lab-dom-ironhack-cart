784e3f306e866f49d68bf3397206c1d5221f7c36
joc <joc@fullsquad.com.br>
2018-11-27 22:20:55 -0200

    half way through

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,5 +1,5 @@
 function deleteItem(e){
-
+  console.log('inside delete button');
 }
 
 function getPriceByProduct(itemNode){
@@ -7,36 +7,31 @@ function getPriceByProduct(itemNode){
 }
 
 function updatePriceByProduct(productPrice, index){
-
+  // obtaining quantity value per product
+  let qty = document.getElementsByClassName('quantity')[index].value;
+  // calculating totalPrice
+  let totalPrice = parseFloat(qty) * parseFloat(productPrice);
+  // adding totalPrice to the DOM
+  let totalPriceElem = document.getElementsByClassName('price-qty');
+  totalPriceElem[index].innerHTML = totalPrice.toFixed(2);
 }
 
 function getTotalPrice() {
+  console.log('inside total price');
 
+  let prices = document.getElementsByClassName('price');
+
+  let pricesArr = [].slice.call(prices);
+
+  let total = 0;
+  pricesArr.forEach((price, index) => {
+    updatePriceByProduct(price.innerHTML, index);
+    let totalPriceByProduct = document.getElementsByClassName('price-qty')[index].innerHTML;
+    total += parseFloat(totalPriceByProduct);
+  });
+  document.getElementById('total').innerHTML = total.toFixed(2);
 }
 
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
 
 window.onload = function(){
   var calculatePriceButton = document.getElementById('calc-prices-button');
@@ -44,9 +39,35 @@ window.onload = function(){
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  // createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
 };
+
+
+
+// function createQuantityInput(){
+
+// }
+
+// function createDeleteButton(){
+
+// }
+
+// function createQuantityNode(){
+
+// }
+
+// function createItemNode(dataType, itemData){
+
+// }
+
+// function createNewItemRow(itemName, itemUnitPrice){
+
+// }
+
+// function createNewItem(){
+
+// }
